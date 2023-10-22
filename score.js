358221c31a13b255f62a972f86520fa26233abe9
Yagna123k <yagna82kb@gmail.com>
2023-10-22 16:23:59 +0530

    Completed

--- score.js
+++ score.js
@@ -1,1 +1,13 @@
 // Iteration 5: Store the player score and display it on the game over screen
+
+var playagain = document.getElementById('play-again-button')
+
+playagain.addEventListener('click', function (){
+    location.href = './game.html'
+})
+
+var scoreBoard = document.getElementById('score-board')
+
+let score = localStorage.getItem('Score')
+
+scoreBoard.textContent = score
